c6f1da496b1c110a3bd188e89d6e1ba16ce6286c
Gabriel8751 <gabriel.inoveh@gmail.com>
2023-10-04 20:19:05 -0300

    Changes API

--- src/app/components/Pokemon.js
+++ src/app/components/Pokemon.js
@@ -2,15 +2,15 @@ import Image from 'next/image'
 
 export default function Pokemon( {pokemon} ) {
     return(
-          <div className="border-[3px] border-green-400 hover" key={pokemon.id}>
+          <div className="border-[3px] border-green-400 rounded-3xl" key={pokemon.id}>
             <span>#{String(pokemon.id).padStart(3, '0')}</span>
-            <h1> {pokemon.name} </h1>
             <Image
             src={pokemon.image}
             width={250}
             height={250}
             alt={pokemon.name}
             />
+            <h1 className='bg-green-300 md.bg-green rounded-b-xl px-2 py-1 text-2xl text-center font-bold capitalize text-white'> {pokemon.name} </h1>
           </div>
     )
 }
