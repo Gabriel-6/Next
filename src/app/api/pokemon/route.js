8e9d7445e5acd14f2e2addd85da12f8ae72d0c86
Gabriel8751 <gabriel.inoveh@gmail.com>
2023-10-03 21:12:42 -0300

    Changes API

---
+++ src/app/api/pokemon/route.js
@@ -0,0 +1,26 @@
+export async function GET(request) {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon/', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await res.json()
+
+      const allPromisses = data.results.map(async (pokemon) => {
+        const res = await fetch(pokemon.url, {
+            headers:{
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await res.json()
+        pokemon.id = data.id
+        pokemon.type = data.types[0].type.name
+        pokemon.image = data.sprites.other['official-artwork'].front_default
+
+        return pokemon
+      })
+
+      await Promise.all(allPromisses)
+     
+      return Response.json({ data })
+}
